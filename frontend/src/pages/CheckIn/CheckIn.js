eb5adbf47cbc0ebd851ad87f8c9983f025cd5c5a
Elin Park <elin@Elins-MBP.attlocal.net>
2022-01-14 15:18:53 -0600

    package-lock.json

--- frontend/src/pages/CheckIn/CheckIn.js
+++ frontend/src/pages/CheckIn/CheckIn.js
@@ -3,8 +3,8 @@ import { useEffect, useState } from "react";
 
 export default function Header() {
   return (
-    <div className="header-container">
-      <h1 className="title-text">Log In</h1>
+    <div className="heading">
+      <h1 className="title-text">Check-In</h1>
     </div>
   );
 }
